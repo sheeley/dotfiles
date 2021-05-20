c648e8a6dc23a92150e3d264af319d90f2fcc533
Johnny Sheeley <sheeley@aigee.org>
2021-05-20 07:16:06 -0700

    Add checks to initial setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,27 +1,32 @@
 #! /usr/bin/env bash
 
-# TODO: test with xcode-select -p
-xcode-select --install
+if ! xcode-select -p; then
+    xcode-select --install
+fi
 
-# TODO: test for brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! which brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # install 1Password and chezmoi - should be all the initial dependencies needed
 brew install 1password-cli chezmoi
 
-# generate key
-# TODO: test if ~/.ssh/id_ed25519 exists
-echo "Enter email to use for ssh key"
-read -r EMAIL
-ssh-keygen -t ed25519 -C "$EMAIL"
-pbcopy < ~/.ssh/id_ed25519.pub
+# generate ssh key
+if [[ ! -f ~/.ssh/id_ed25519 ]]; then
+    echo "Enter email to use for ssh key"
+    read -r EMAIL
+    ssh-keygen -t ed25519 -C "$EMAIL"
+fi
 
+# store key in github
+pbcopy < ~/.ssh/id_ed25519.pub
 echo "save your key in Github and and generate a token"
 open https://github.com/settings/keys
 open https://github.com/settings/tokens
 
-# login to 1password
-eval "$(op signin my)"
+# login to 1password, setting token for futher usage
+# shellcheck disable=2034
+OP_TOKEN=$(op signin my.1password.com "$EMAIL")
 
 # run the actual setup
 chezmoi init --apply git@github.com:sheeley/dotfiles.git
