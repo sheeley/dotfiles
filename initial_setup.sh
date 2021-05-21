e2dc6c7ed01ecf95e5b5532d3c07bf6fb2cc42fc
Johnny Sheeley <sheeley@aigee.org>
2021-05-20 23:05:59 -0700

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,8 +1,18 @@
 #! /usr/bin/env bash
 
+confirm() {
+    echo "Continue?"
+    read -r CONFIRM
+    if [ "$CONFIRM" -ne "y" ]; then
+        return 1
+    fi
+}
+
 if ! xcode-select -p; then
     xcode-select --install
 fi
+
+confirm || exit 1
 
 if ! which brew; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,6 +21,8 @@ fi
 # install 1Password and chezmoi - should be all the initial dependencies needed
 sh -c "$(curl -fsLS git.io/chezmoi)"
 brew install 1password 1password-cli
+
+confirm || exit 1
 
 # generate ssh key
 if [[ ! -f ~/.ssh/id_ed25519 ]]; then
@@ -25,9 +37,16 @@ echo "save your key in Github and and generate a token"
 open https://github.com/settings/keys
 open https://github.com/settings/tokens
 
+if [ "$HOMEBREW_GITHUB_API_TOKEN" -eq "" ]; then
+    echo "Enter email to use for ssh key"
+    read -r HOMEBREW_GITHUB_API_TOKEN
+    export HOMEBREW_GITHUB_API_TOKEN
+fi
+
 # login to 1password, setting token for futher usage
 # shellcheck disable=2034
 OP_TOKEN=$(op signin my.1password.com "$EMAIL")
 
+confirm || exit 1
 # run the actual setup
 ~/bin/chezmoi init --apply git@github.com:sheeley/dotfiles.git
