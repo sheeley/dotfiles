807c1e38b2a8ac1688aa0475367ec98a67c69eaa
Johnny Sheeley <sheeley@aigee.org>
2021-05-21 00:23:31 -0700

    Update run_once_before_brew_packages_personal.sh

--- run_once_before_brew_packages_personal.sh
+++ run_once_before_brew_packages_personal.sh
@@ -1,17 +1,24 @@
 #! /usr/bin/env bash
 
+# swiftlint needs to come after xcode
 brew bundle --file=- <<-EOS
 brew "borgbackup"
 brew "hugo"
 brew "rclone"
 brew "rsync"
-brew "swiftlint"
-brew "swiftformat"
-cask "swiftformat-for-xcode"
 mas "Space Gremlin", id: 414515628
 mas "Reeder", id: 1529448980
 mas "Twitter", id: 1482454543
 mas "Watchdog", id: 734258109
-mas "WhatsApp", id: 1147396723
-mas "Xcode", id: 497799835
 EOS
+
+echo -n "Install Xcode/swift? "
+read -r INSTALL
+if [ "$INSTALL" == "y"]; then
+  brew bundle --file=- <<-EOS
+  mas "Xcode", id: 497799835
+  brew "swiftlint"
+  brew "swiftformat"
+  cask "swiftformat-for-xcode"
+EOS
+fi
