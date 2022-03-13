bf2d7ccf26887e3df9fb83cf84da50de290484ef
Johnny Sheeley <sheeley@aigee.org>
2022-03-13 12:41:23 -0700

    Update brew packages for global install

--- run_once_before_brew_packages.sh
+++ run_once_before_brew_packages.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
+
 if [ "$VIM" != "" ]; then
     echo "No install within VIM"
     exit 0
 fi
 
 brew bundle --file=- <<-EOS
-tap "afnanenayet/tap"
 tap "homebrew/bundle"
 tap "homebrew/cask"
 tap "homebrew/cask-fonts"
@@ -13,40 +13,31 @@ tap "homebrew/core"
 tap "muesli/tap"
 brew "atool"
 brew "bat"
-brew "diffsitter"
-brew "git-delta"
+brew "broot"
+brew "dust"
+brew "entr"
+brew "fd"
 brew "fish"
 brew "fzf"
 brew "git"
+brew "git-delta"
 brew "gitui"
 brew "go"
 brew "gron"
-brew "hub"
-brew "hugo"
 brew "jid"
 brew "jq"
-brew "lunchy"
-brew "m-cli"
-brew "mas"
 brew "neovim"
-brew "nushell"
 brew "pv"
-brew "python@3.9"
 brew "ripgrep"
-brew "saulpw/vd/visidata"
 brew "shellcheck"
 brew "shfmt"
 brew "ssh-copy-id"
 brew "starship"
-brew "terraform"
 brew "the_silver_searcher"
 brew "tldr"
-brew "tree"
 brew "vint"
 brew "wget"
-brew "yarn"
 brew "muesli/tap/duf"
-cask "1password-cli"
 cask "bartender"
 cask "diffmerge"
 cask "font-fira-code-nerd-font"
@@ -55,15 +46,15 @@ cask "font-profont-nerd-font"
 cask "font-space-mono-nerd-font"
 cask "iterm2"
 cask "krisp"
-cask "qlstephen"
 cask "shiftit"
 mas "Amphetamine", id: 937984704
 mas "Clocker", id: 1056643111
 mas "Expressions", id: 913158085
 mas "iA Writer", id: 775737590
+mas "Kindle", id: 405399194
 mas "Patterns", id: 429449079
-mas "The Unarchiver", id: 425424353
-mas "Slack for Desktop", id: 803453959
+mas "Slack", id: 803453959
+mas "The Unarchiver", id: 425424353 
 EOS
 
 if ! pip3 show neovim2 &>/dev/null; then
