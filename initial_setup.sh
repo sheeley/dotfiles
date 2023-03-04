a7339ddb127b80b5232be9868c7178122417afb9
Johnny Sheeley <sheeley@aigee.org>
2023-03-04 09:04:24 -0800

    Install nix

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,52 +3,52 @@
 export PATH=$PATH:~/bin
 
 confirm() {
-    echo -n "Continue?"
-    read -r CONFIRM
-    if [ "$CONFIRM" != "y" ] && [ "$CONFIRM" != "yes" ]; then
-        return 1
-    fi
+	echo -n "Continue?"
+	read -r CONFIRM
+	if [ "$CONFIRM" != "y" ] && [ "$CONFIRM" != "yes" ]; then
+		return 1
+	fi
 }
 
 EMAIL=""
 # TODO: XCode doesn't like ed25519. Maybe generate more than one? Hrm.
 # generate ssh key
 if [ ! -f ~/.ssh/id_ed25519 ]; then
-    if [ "$EMAIL" == "" ]; then
-        echo "Enter email to use for ssh key"
-        read -r EMAIL
-    fi
+	if [ "$EMAIL" == "" ]; then
+		echo "Enter email to use for ssh key"
+		read -r EMAIL
+	fi
 
-    ssh-keygen -t ed25519 -C "$EMAIL"
-    eval "$(ssh-agent -s)"
-    if ! ssh-add -l -E sha256 | grep ED25519; then
-        ssh-add -K ~/.ssh/id_ed25519
-    fi
-    # store key in github
-    echo "Open Github to create token and save SSH key?"
-    if confirm; then
-        pbcopy <~/.ssh/id_ed25519.pub
-        open 'https://github.com/settings/tokens/new?scopes=gist,public_repo,workflow&description=Homebrew'
-        open https://github.com/settings/keys
-    fi
+	ssh-keygen -t ed25519 -C "$EMAIL"
+	eval "$(ssh-agent -s)"
+	if ! ssh-add -l -E sha256 | grep ED25519; then
+		ssh-add -K ~/.ssh/id_ed25519
+	fi
+	# store key in github
+	echo "Open Github to create token and save SSH key?"
+	if confirm; then
+		pbcopy <~/.ssh/id_ed25519.pub
+		open 'https://github.com/settings/tokens/new?scopes=gist,public_repo,workflow&description=Homebrew'
+		open https://github.com/settings/keys
+	fi
 
-    if [ "$HOMEBREW_GITHUB_API_TOKEN" == "" ]; then
-        echo "Enter Github token"
-        read -r HOMEBREW_GITHUB_API_TOKEN
-        export HOMEBREW_GITHUB_API_TOKEN
-    fi
+	if [ "$HOMEBREW_GITHUB_API_TOKEN" == "" ]; then
+		echo "Enter Github token"
+		read -r HOMEBREW_GITHUB_API_TOKEN
+		export HOMEBREW_GITHUB_API_TOKEN
+	fi
 
 else
-    EMAIL=$(cut -d' ' -f3 <~/.ssh/id_ed25519.pub)
+	EMAIL=$(cut -d' ' -f3 <~/.ssh/id_ed25519.pub)
 fi
 
 if ! xcode-select -p; then
-    xcode-select --install
-    confirm || exit 1
+	xcode-select --install
+	confirm || exit 1
 fi
 
 if ! which brew; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -57,17 +57,17 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install 1password 1password-cli mas
 
 if ! mas account; then
-    echo "Sign in to app store"
-    mas open
-    confirm || exit 1
+	echo "Sign in to app store"
+	mas open
+	confirm || exit 1
 fi
 
-# if ! which nix; then
-#     curl -L https://nixos.org/nix/install | sh || exit 1
-# fi
+if ! which nix; then
+	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+fi
 
 if ! which chezmoi; then
-    sh -c "$(curl -fsLS git.io/chezmoi)" || exit 1
+	sh -c "$(curl -fsLS git.io/chezmoi)" || exit 1
 fi
 
 # login to 1password, setting token for futher usage
