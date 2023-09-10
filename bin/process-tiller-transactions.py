8b2613460d743187b231c72324b80d628948a938
Johnny Sheeley <sheeley@aigee.org>
2023-09-09 18:17:35 -0700

    Add tiller transaction script

---
+++ bin/process-tiller-transactions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from csv import DictReader
+from decimal import Decimal
+from collections import defaultdict
+
+total = Decimal()
+count = defaultdict(int)
+
+with open("Tiller Finance Tracker - Transactions.csv", "r") as f:
+    r = DictReader(f)
+    for row in r:
+        if row.get("Category") != "Finance: Transfer":
+            continue
+        if not row["Date"].endswith("2023"):
+            break
+        amt = row["Amount"].replace("$", "").replace(",", "")
+        amount = Decimal(amt)
+        total += amount
+        count[amount] += 1
+        print(row["Description"], amount, total)
+
+print(count)
