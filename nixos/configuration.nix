a14eca0244e646cc9bcc7d9db155cef6ae7011e6
Johnny Sheeley <sheeley@aigee.org>
2024-04-01 20:30:46 -0700

    Attempt explicit ports

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   # config,
-  pkgs,
+  # pkgs,
   ...
 }: {
   imports = [
--- nixos/home-assistant.nix
+++ nixos/home-assistant.nix
@@ -1,15 +1,33 @@
 {...}: {
-  networking.firewall.allowedUDPPorts = [5353];
-  networking.firewall.allowedTCPPorts = [8123 21063];
+  networking.firewall.allowedUDPPorts = [
+    # homekit
+    5353
+  ];
+
+  networking.firewall.allowedTCPPorts = [
+    8123
+    # homekit
+    21063
+  ];
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
-      volumes = ["home-assistant:/config"];
-      environment.TZ = "America/Los_Angeles";
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+      volumes = ["home-assistant:/config"];
+
+      environment.TZ = "America/Los_Angeles";
       extraOptions = [
         "--network=host"
         # "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
+      ];
+
+      ports = [
+        "8123:8123"
+
+        # homekit
+        "5353:5353"
+        "21063:21063"
       ];
     };
   };
