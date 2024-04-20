4e04643b6ad93e5a55495c7f30a8df8d59bcb797
Johnny Sheeley <sheeley@aigee.org>
2024-04-20 13:17:40 -0700

    Iterating on Hyundai and Hatch components

--- nixos/home-assistant.nix
+++ nixos/home-assistant.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  buildHomeAssistantComponent,
+  pkgs,
+  ...
+}: {
   networking.firewall.allowedUDPPorts = [
     # homekit
     5353
@@ -9,7 +13,6 @@
     # homekit
     21063
   ];
-
   # virtualisation.oci-containers = {
   #   backend = "podman";
   #   containers.homeassistant = {
@@ -37,11 +40,38 @@
   services.home-assistant = {
     enable = true;
     extraComponents = [
-      # Components required to complete the onboarding
-      # "esphome"
-      # "met"
-      # "radio_browser"
+      # from https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/home-assistant/component-packages.nix
+      "default_config"
+      "met"
+      "esphome"
+
+      # custom
+      "androidtv_remote"
+      "apple_tv"
+      "anthemav"
+      "backup"
+      "bluetooth"
+      "bluetooth_adapters"
+      "bluetooth_le_tracker"
+      "bluetooth_tracker"
+      "cast"
+      "elgato"
+      "homekit"
+      "homekit_controller"
+      "ios"
+      "ipp"
+      # "lovelace"
+      "mobile_app"
+      "opower"
+      "pge"
+      "plex"
+      "spotify"
+      "tailscale"
+      "unifi"
+      "unifi_direct"
+      "unifiprotect"
     ];
+
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
@@ -51,17 +81,51 @@
         default = "info";
       };
 
-      frontend = {
-        themes = "themes";
+      homeassistant = {
+        name = "Home";
+
+        latitude = "!secret latitude";
+        longitude = "!secret longitude";
+        elevation = "!secret elevation";
+        country = "US";
+
+        unit_system = "imperial";
+        time_zone = "America/Los_Angeles";
       };
-
-      automation = {
-
-      };
-
-      script = {};
-
-      scene = {};
     };
+
+    customComponents = [
+      (buildHomeAssistantComponent {
+        owner = "dahlb";
+        domain = "ha_hatch";
+        version = "1.17.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "dahlb";
+          repo = "ha_hatch";
+          rev = "v1.17.1";
+          sha256 = "sha256-YUBBSoPMe5bghsUJ2jpRHWvLQftkgWNaMuHqTZ5wZQg=";
+        };
+
+        installPhase = ''
+          cp -r custom_components/ha_hatch $out
+        '';
+      })
+
+      (buildHomeAssistantComponent {
+        owner = "Hyundai-Kia-Connect";
+        domain = "kia_uvo";
+        version = "2.24.2";
+        src = pkgs.fetchFromGitHub {
+          owner = "Hyundai-Kia-Connect";
+          repo = "kia_uvo";
+          rev = "v2.24.2";
+          sha256 = "sha256-i9P7Po6fqxbTsZMHw6YSBfQkIasF38y0Ru9kWj0RyEk=";
+        };
+
+        installPhase = ''
+          cp -r custom_components/kia_uvo $out
+        '';
+      })
+    ];
   };
 }
