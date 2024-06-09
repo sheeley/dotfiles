dc37e5fb60faf0ec1b081e85379264176f57bf3d
Johnny Sheeley <sheeley@aigee.org>
2024-06-09 07:49:21 -0700

    Disable hostname check for content cache

--- homebase/content-cache.bash
+++ homebase/content-cache.bash
@@ -1,7 +1,8 @@
-HN=$(hostname_wrapper)
-if [[ "$HN" = "homebase" ]]; then
-	sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist Active -bool YES
-	sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist CacheLimit -int 50000000000
-	sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist KeepAwake -bool YES
-	sudo AssetCacheManagerUtil reloadSettings
-fi
+# this is ONLY included in homebase.nix now, so unnecessary.
+# HN=$(hostname_wrapper)
+# if [[ "$HN" = "homebase" ]]; then
+sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist Active -bool YES
+sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist CacheLimit -int 50000000000
+sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist KeepAwake -bool YES
+sudo AssetCacheManagerUtil reloadSettings
+# fi
