f74482e84acbac7126dd2f37a2dddffe060aac1f
Johnny Sheeley <sheeley@aigee.org>
2024-07-28 15:27:37 -0700

    Clean up lab-borg

--- lab/lab-borg.nix
+++ lab/lab-borg.nix
@@ -1,12 +1,10 @@
 {
-  pkgs,
   private,
   user,
   ...
 }: {
   services.borgbackup.jobs.homelab = {
     paths = [
-      # /mnt/Media
       (builtins.toString /mnt/Media)
     ];
     exclude = [
@@ -21,73 +19,4 @@
     compression = "auto,zstd";
     startAt = "daily";
   };
-
-  # home-manager.users.${user} = {
-  #   home.packages = [
-  #     pkgs.borgmatic
-  #   ];
-  #
-  #   programs.borgmatic = {
-  #     enable = true;
-  #
-  #     backups = {
-  #       lab = {
-  #         location = {
-  #           repositories = [
-  #             "ssh://${private.borgUser}@${private.borgUser}.repo.borgbase.com/./repo"
-  #           ];
-  #
-  #           sourceDirectories = [
-  #             "/mnt/Media"
-  #             "/home/${user}"
-  #           ];
-  #
-  #           excludeHomeManagerSymlinks = true;
-  #         };
-  #
-  #         retention = {
-  #           keepDaily = 7;
-  #           keepMonthly = 3;
-  #           keepWeekly = 4;
-  #           keepYearly = 3;
-  #         };
-  #
-  #         storage.encryptionPasscommand = "echo ${private.borgSecret}";
-  #
-  #         consistency.checks = [
-  #           {
-  #             name = "repository";
-  #             frequency = "2 weeks";
-  #           }
-  #           {
-  #             name = "archives";
-  #             frequency = "4 weeks";
-  #           }
-  #           {
-  #             name = "data";
-  #             frequency = "6 weeks";
-  #           }
-  #           {
-  #             name = "extract";
-  #             frequency = "6 weeks";
-  #           }
-  #         ];
-  #       };
-  #     };
-  #   };
-  #
-  #   systemd.user.services.borgmatic = {
-  #     serviceConfig = {
-  #       Type = "oneshot";
-  #       ExecStart = "${pkgs.borgmatic}/bin/borgmatic --verbosity 1";
-  #     };
-  #
-  #     timerConfig = {
-  #       OnCalendar = "daily";
-  #       Persistent = true;
-  #     };
-  #
-  #     # wantedBy = ["timers.target"];
-  #   };
-  # };
 }
