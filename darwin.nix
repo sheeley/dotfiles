5ac8b526942b474a4db09aedfcba60dfba9a2d02
Johnny Sheeley <sheeley@aigee.org>
2024-09-15 10:55:09 -0700

    Simplify handling local nix cache issues

--- darwin.nix
+++ darwin.nix
@@ -9,15 +9,30 @@
   #   ./dock.nix
   # ];
 
+  # Add substituter for local cache
+  nix.settings.substituters = ["http://lab.aigee.org" "http://192.168.1.17"];
+  # nix fails on the _first_ failure instead of falling back by default
+  nix.extraOptions = ''
+    # Ensure we can still build when missing-server is not accessible
+    fallback = true
+  '';
+
   home-manager.users.${user} = {...}: {
     home.packages = [
       # TODO: see above
       # pkgs.dockutil
       pkgs.obsidian
     ];
+
+    programs.ssh = {
+      extraConfig = ''
+        # Ensure the local cache fails fast so fallback can happen
+        Host lab.aigee.org
+            ConnectTimeout 3
+      '';
+    };
   };
 
-  nix.settings.substituters = ["http://lab.aigee.org" "http://192.168.1.17"];
   services.nix-daemon.enable = true;
 
   fonts.packages = [
