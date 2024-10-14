be13945e15df99d1b2657f4d963a8d007761afa2
Johnny Sheeley <sheeley@aigee.org>
2024-10-13 20:56:13 -0700

    Let's see if this auto-attaches

--- programs/fish.nix
+++ programs/fish.nix
@@ -22,6 +22,9 @@
 
       test -e {$HOME}/.iterm2_shell_integration.fish
       and source {$HOME}/.iterm2_shell_integration.fish
+
+      test -n "$SSH_CONNECTION"
+      and zellij attach --create
     '';
 
     loginShellInit = "fish_add_path --move --prepend --path $HOME/.nix-profile/bin /run/wrappers/bin /etc/profiles/per-user/$USER/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin";
