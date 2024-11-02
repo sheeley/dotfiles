f4f403143e82d56847237ff6e3b1006513c08019
Johnny Sheeley <sheeley@aigee.org>
2024-11-02 09:19:21 -0700

    Move zellij to its own file

--- home.nix
+++ home.nix
@@ -41,6 +41,7 @@ in {
         ./programs/ssh.nix
         ./programs/starship.nix
         ./programs/vscode.nix
+        ./programs/zellij.nix
         ./programs/zsh.nix
       ]
       # ++ ((lib.optionals (lib.hasAttr "personal" private && private.personal)) [
@@ -49,37 +50,6 @@ in {
       ++ ((lib.optionals isMac) [
         ./home-darwin-defaults.nix
       ]);
-
-    programs.zellij = {
-      enable = true;
-      enableBashIntegration = false;
-      enableFishIntegration = false;
-      enableZshIntegration = false;
-      settings = {
-        copy_command = "pbcopy";
-      };
-      #       keybinds {
-      #     // keybinds are divided into modes
-      #     normal {
-      #         // bind instructions can include one or more keys (both keys will be bound separately)
-      #         // bind keys can include one or more actions (all actions will be performed with no sequential guarantees)
-      #         bind "Ctrl g" { SwitchToMode "locked"; }
-      #         bind "Ctrl p" { SwitchToMode "pane"; }
-      #         bind "Alt n" { NewPane; }
-      #         bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
-      #     }
-      #     pane {
-      #         bind "h" "Left" { MoveFocus "Left"; }
-      #         bind "l" "Right" { MoveFocus "Right"; }
-      #         bind "j" "Down" { MoveFocus "Down"; }
-      #         bind "k" "Up" { MoveFocus "Up"; }
-      #         bind "p" { SwitchFocus; }
-      #     }
-      #     locked {
-      #         bind "Ctrl g" { SwitchToMode "normal"; }
-      #     }
-      # }
-    };
 
     home = {
       stateVersion = "22.05";
