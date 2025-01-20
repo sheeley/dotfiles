fa86a80feb5f9349f3ad9bd99fe9b1284449c52a
Johnny Sheeley <sheeley@aigee.org>
2025-01-20 15:31:11 -0800

    Up the mac content cache to 100Gb

--- homebase/content-cache.bash
+++ homebase/content-cache.bash
@@ -2,7 +2,8 @@
 # HN=$(hostname_wrapper)
 # if [[ "$HN" = "homebase" ]]; then
 sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist Active -bool YES
-sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist CacheLimit -int 50000000000
+# 100GB
+sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist CacheLimit -int 100000000000
 sudo -u _assetcache defaults write /Library/Preferences/com.apple.AssetCache.plist KeepAwake -bool YES
 sudo AssetCacheManagerUtil reloadSettings
 # fi
