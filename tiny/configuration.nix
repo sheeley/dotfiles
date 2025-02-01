12379dc6af396ecfcd682a87557e31f1780e9158
Johnny Sheeley <sheeley@aigee.org>
2025-02-01 06:39:43 -0800

    Enable CoreDNS

--- tiny/configuration.nix
+++ tiny/configuration.nix
@@ -17,4 +17,13 @@
   networking.hostName = "tiny"; # Define your hostname.
   # Define a user account. Don't forget to set a password with ‘passwd’.
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  services.coredns = {
+    enable = true;
+    config = ''
+      local.aigee.org {
+        mdns local.aigee.org
+      }
+    '';
+  };
 }
