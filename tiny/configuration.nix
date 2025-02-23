f6a993a7e8b111a725d0111a0671c486a3e4d426
Johnny Sheeley <sheeley@aigee.org>
2025-02-22 22:08:10 -0800

    Add CoreDNS

--- tiny/configuration.nix
+++ tiny/configuration.nix
@@ -3,7 +3,18 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  corednsMDNS = pkgs.coredns.override {
+    externalPlugins = [
+      {
+        name = "mdns";
+        repo = "github.com/openshift/coredns-mdns";
+        version = "master";
+      }
+    ];
+    vendorHash = "sha256-CocHwNw6h6RJHkIarC7+ifHl7PmbodXwTeT99Jeg+xo=";
+  };
+in {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -20,6 +31,7 @@
 
   services.coredns = {
     enable = true;
+    package = corednsMDNS;
     config = ''
       local.aigee.org {
         mdns local.aigee.org
