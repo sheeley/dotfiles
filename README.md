0880332b43758cb979096d4c403ac1b3ded64fcb
Johnny Sheeley <sheeley@aigee.org>
2025-03-01 08:23:51 -0800

    Update

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 ```sh
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/sheeley/dotfiles/refs/heads/main/initial_setup.sh)"
 ```
+
+https://github.com/bluskript/nix-inspect
+https://github.com/oppiliappan/statix
+
+https://github.com/nix-community/nix-init
+https://github.com/nix-community/nurl
+
+nix-output-monitor
+https://github.com/viperML/nh
+
+https://github.com/astro/deadnix
+https://github.com/symphorien/nix-du
--- programs/vscode.nix
+++ programs/vscode.nix
@@ -1,24 +1,26 @@
 {pkgs, ...}: {
   programs.vscode = {
-    enable = false;
-    enableExtensionUpdateCheck = false;
-    enableUpdateCheck = false;
-    # package = pkgs.vscodium;
+    enable = true;
+    profiles.default = {
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
+      # package = pkgs.vscodium;
 
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      christian-kohler.path-intellisense
-      esbenp.prettier-vscode
-      hashicorp.terraform
-      jnoortheen.nix-ide
-      kamadorueda.alejandra
-      timonwong.shellcheck
-    ];
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        christian-kohler.path-intellisense
+        esbenp.prettier-vscode
+        hashicorp.terraform
+        jnoortheen.nix-ide
+        kamadorueda.alejandra
+        timonwong.shellcheck
+      ];
 
-    userSettings = {
-      "editor.minimap.enabled" = false;
-      "editor.formatOnSave" = true;
-      "extensions.ignoreRecommendations" = true;
+      userSettings = {
+        "editor.minimap.enabled" = false;
+        "editor.formatOnSave" = true;
+        "extensions.ignoreRecommendations" = true;
+      };
     };
   };
 }
