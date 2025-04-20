46379a16d8bf026480d4a0a60486aebd3f410a62
Johnny Sheeley <sheeley@aigee.org>
2025-04-20 15:38:57 -0700

    Disable zellij tips

--- programs/zellij.nix
+++ programs/zellij.nix
@@ -6,6 +6,7 @@
     enableZshIntegration = false;
     settings = {
       copy_command = "pbcopy";
+      show_startup_tips = "false";
       # default_layout = "compact";
     };
     #       keybinds {
