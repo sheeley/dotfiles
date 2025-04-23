84c38393162dc793997a1831fccd63c59e78d267
Johnny Sheeley <sheeley@aigee.org>
2025-04-22 21:39:52 -0700

    Organize plugins

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -68,17 +68,52 @@ in {
       # lazyredraw = true;
     };
 
-    plugins.cmp-path = {enable = true;};
-    plugins.toggleterm = {
-      enable = true;
-      settings = {
-        open_mapping = "[[<c-/>]]";
-        start_in_insert = true;
-      };
-    };
+    plugins = {
+      hop = {enable = true;};
+      cmp-path = {enable = true;};
 
-    plugins.nix = {
-      enable = true;
+      toggleterm = {
+        enable = true;
+        settings = {
+          open_mapping = "[[<c-/>]]";
+          start_in_insert = true;
+        };
+      };
+
+      nix = {
+        enable = true;
+      };
+      todo-comments = {
+        enable = true;
+      };
+
+      none-ls = {
+        enable = true;
+        sources = {
+          diagnostics = {
+            # shellcheck.enable = true;
+            # cppcheck.enable = true;
+            gitlint.enable = true;
+          };
+          #        code_actions = {
+          #          shellcheck.enable = true;
+          #        };
+          formatting = {
+            alejandra.enable = true;
+            black.enable = true;
+            # stylua.enable = true;
+            goimports.enable = true;
+            cbfmt.enable = true;
+            shfmt.enable = true;
+            # taplo.enable = true;
+            prettier.enable = true;
+            swiftformat = {
+              enable = true;
+              package = null;
+            };
+          };
+        };
+      };
     };
 
     autoCmd = [
@@ -189,38 +224,6 @@ in {
       }
     ];
 
-    plugins.todo-comments = {
-      enable = true;
-    };
-
-    plugins.none-ls = {
-      enable = true;
-      sources = {
-        diagnostics = {
-          # shellcheck.enable = true;
-          # cppcheck.enable = true;
-          gitlint.enable = true;
-        };
-        #        code_actions = {
-        #          shellcheck.enable = true;
-        #        };
-        formatting = {
-          alejandra.enable = true;
-          black.enable = true;
-          # stylua.enable = true;
-          goimports.enable = true;
-          cbfmt.enable = true;
-          shfmt.enable = true;
-          # taplo.enable = true;
-          prettier.enable = true;
-          swiftformat = {
-            enable = true;
-            package = null;
-          };
-        };
-      };
-    };
-
     plugins.luasnip = {
       enable = true;
     };
@@ -237,307 +240,309 @@ in {
       local luasnip = require("luasnip")
     '';
 
-    plugins.cmp = {
-      enable = true;
-      autoEnableSources = true;
-      settings = {
-        mapping = {
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-e>" = "cmp.mapping.close()";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          # "<Tab>" = {
-          #   action = ''
-          #     function(fallback)
-          #       if cmp.visible() then
-          #         cmp.select_next_item()
-          #       elseif luasnip.expandable() then
-          #         luasnip.expand()
-          #       elseif luasnip.expand_or_jumpable() then
-          #         luasnip.expand_or_jump()
-          #       elseif check_backspace() then
-          #         fallback()
-          #       else
-          #         fallback()
-          #       end
-          #     end
-          #   '';
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          # modes = ["i" "s"];
-          # };
-        };
-        snippet.expand = ''
-          function(args)
-            require('luasnip').lsp_expand(args.body)
-          end
-        '';
-
-        completion = {
-          autocomplete = [
-            "require('cmp.types').cmp.TriggerEvent.TextChanged"
-          ];
-        };
-
-        sources = [
-          {name = "nvim_lsp";}
-          {name = "path";}
-          {name = "buffer";}
-        ];
-      };
-    };
-
-    # mapping = {
-    #   "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-    #   "<C-f>" = "cmp.mapping.scroll_docs(4)";
-    #   "<C-Space>" = "cmp.mapping.complete()";
-    #   "<C-e>" = "cmp.mapping.close()";
-    #   "<Tab>" = {
-    #     modes = ["i" "s"];
-    #     action = "cmp.mapping.select_next_item()";
-    #   };
-    #   "<S-Tab>" = {
-    #     modes = ["i" "s"];
-    #     action = "cmp.mapping.select_prev_item()";
-    #   };
-    # "<CR>" = "cmp.mapping.confirm({ select = true })";
-    # };
-
-    plugins.web-devicons = {
-      enable = true;
-    };
-
-    plugins.telescope = {
-      enable = true;
-      enabledExtensions = ["ui-select"];
-      # extensionConfig = {
-      #   ui-select = {
-      #     __raw = ''
-      #       require("telescope.themes").get_dropdown {
-      #         -- even more opts
-      #       }
-      #     '';
-      #   };
-      # };
-    };
-
-    plugins.treesitter = {
-      enable = true;
-      settings = {
-        indent = {
-          enable = true;
-        };
-      };
-
-      grammarPackages = with config.programs.nixvim.plugins.treesitter.package.passthru.builtGrammars; [
-        arduino
-        bash
-        c
-        cpp
-        cuda
-        dart
-        devicetree
-        diff
-        dockerfile
-        gitattributes
-        gitignore
-        git_rebase
-        # help
-        html
-        json
-        markdown
-        markdown_inline
-        nix
-        # python
-        regex
-        toml
-        vim
-        # yaml
-      ];
-    };
-
-    plugins.treesitter-refactor = {
-      enable = true;
-      highlightDefinitions = {
-        enable = true;
-        clearOnCursorMove = true;
-      };
-      smartRename = {
-        enable = true;
-      };
-      navigation = {
-        enable = true;
-      };
-    };
-
-    plugins.treesitter-context = {
-      enable = true;
-    };
-
-    plugins.vim-matchup = {
-      treesitter = {
-        enable = true;
-        include_match_words = true;
-      };
-      enable = true;
-    };
-
-    plugins.comment = {
-      enable = true;
-    };
-
-    plugins.neo-tree = {
-      enable = true;
-
-      closeIfLastWindow = true;
-      enableGitStatus = false;
-      filesystem.filteredItems = {
-        hideDotfiles = false;
-        # hideGitIgnored = true;
-        visible = true;
-      };
-    };
-
-    plugins.plantuml-syntax.enable = true;
-
-    plugins.indent-blankline = {
-      enable = true;
-
-      # useTreesitter = true;
-      settings = {
-        scope = {
-          enabled = true;
-          show_start = true;
-        };
-      };
-    };
-
-    plugins.lsp = {
-      enable = true;
-
-      keymaps = {
-        silent = true;
-
-        lspBuf = {
-          "gd" = "definition";
-          "gD" = "declaration";
-          "ca" = "code_action";
-          "ff" = "format";
-          "K" = "hover";
-        };
-      };
-
-      servers = {
-        nil_ls = {
-          enable = true;
-          settings = {
-            formatting.command = ["alejandra" "--quiet"];
-          };
-        };
-        bashls.enable = true;
-        helm_ls.enable = true;
-        nushell.enable = true;
-        # sourcekit.enable = true;
-        terraformls.enable = true;
-        yamlls.enable = true;
-        jsonls.enable = true;
-        gopls.enable = true;
-        eslint.enable = true;
-        ruff.enable = true;
-        # dartls.enable = true;
-      };
-    };
-
-    plugins.lsp-format = {
-      enable = true;
-    };
-
-    # vim.lsp.set_log_level("debug")
-    # extraConfigLua = ''
-    #   require'lspconfig'.sourcekit.setup {
-    #     cmd = {"/usr/bin/sourcekit-lsp"}
-    #   }
-    # '';
-
-    plugins.lspkind = {
-      enable = true;
+    plugins = {
       cmp = {
         enable = true;
+        autoEnableSources = true;
+        settings = {
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            # "<Tab>" = {
+            #   action = ''
+            #     function(fallback)
+            #       if cmp.visible() then
+            #         cmp.select_next_item()
+            #       elseif luasnip.expandable() then
+            #         luasnip.expand()
+            #       elseif luasnip.expand_or_jumpable() then
+            #         luasnip.expand_or_jump()
+            #       elseif check_backspace() then
+            #         fallback()
+            #       else
+            #         fallback()
+            #       end
+            #     end
+            #   '';
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            # modes = ["i" "s"];
+            # };
+          };
+          snippet.expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
+
+          completion = {
+            autocomplete = [
+              "require('cmp.types').cmp.TriggerEvent.TextChanged"
+            ];
+          };
+
+          sources = [
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+          ];
+        };
       };
-    };
 
-    plugins.nvim-lightbulb = {
-      enable = true;
-      settings = {
-        autocmd.enabled = true;
+      # mapping = {
+      #   "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+      #   "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      #   "<C-Space>" = "cmp.mapping.complete()";
+      #   "<C-e>" = "cmp.mapping.close()";
+      #   "<Tab>" = {
+      #     modes = ["i" "s"];
+      #     action = "cmp.mapping.select_next_item()";
+      #   };
+      #   "<S-Tab>" = {
+      #     modes = ["i" "s"];
+      #     action = "cmp.mapping.select_prev_item()";
+      #   };
+      # "<CR>" = "cmp.mapping.confirm({ select = true })";
+      # };
+
+      web-devicons = {
+        enable = true;
       };
-    };
 
-    plugins.inc-rename = {
-      enable = true;
-    };
-
-    plugins.lualine = {
-      enable = true;
-    };
-
-    plugins.trouble = {
-      enable = true;
-      settings = {
-        auto_close = true;
+      telescope = {
+        enable = true;
+        enabledExtensions = ["ui-select"];
+        # extensionConfig = {
+        #   ui-select = {
+        #     __raw = ''
+        #       require("telescope.themes").get_dropdown {
+        #         -- even more opts
+        #       }
+        #     '';
+        #   };
+        # };
       };
-    };
 
-    plugins.noice = {
-      enable = true;
-
-      settings = {
-        messages = {
-          view = "mini";
-          viewError = "mini";
-          viewWarn = "mini";
+      treesitter = {
+        enable = true;
+        settings = {
+          indent = {
+            enable = true;
+          };
         };
 
-        lsp.override = {
-          "vim.lsp.util.convert_input_to_markdown_lines" = true;
-          "vim.lsp.util.stylize_markdown" = true;
-          "cmp.entry.get_documentation" = true;
+        grammarPackages = with config.programs.nixvim.plugins.treesitter.package.passthru.builtGrammars; [
+          arduino
+          bash
+          c
+          cpp
+          cuda
+          dart
+          devicetree
+          diff
+          dockerfile
+          gitattributes
+          gitignore
+          git_rebase
+          # help
+          html
+          json
+          markdown
+          markdown_inline
+          nix
+          # python
+          regex
+          toml
+          vim
+          # yaml
+        ];
+      };
+
+      treesitter-refactor = {
+        enable = true;
+        highlightDefinitions = {
+          enable = true;
+          clearOnCursorMove = true;
+        };
+        smartRename = {
+          enable = true;
+        };
+        navigation = {
+          enable = true;
+        };
+      };
+
+      treesitter-context = {
+        enable = true;
+      };
+
+      vim-matchup = {
+        treesitter = {
+          enable = true;
+          include_match_words = true;
+        };
+        enable = true;
+      };
+
+      comment = {
+        enable = true;
+      };
+
+      neo-tree = {
+        enable = true;
+
+        closeIfLastWindow = true;
+        enableGitStatus = false;
+        filesystem.filteredItems = {
+          hideDotfiles = false;
+          # hideGitIgnored = true;
+          visible = true;
+        };
+      };
+
+      plantuml-syntax.enable = true;
+
+      indent-blankline = {
+        enable = true;
+
+        # useTreesitter = true;
+        settings = {
+          scope = {
+            enabled = true;
+            show_start = true;
+          };
+        };
+      };
+
+      lsp = {
+        enable = true;
+
+        keymaps = {
+          silent = true;
+
+          lspBuf = {
+            "gd" = "definition";
+            "gD" = "declaration";
+            "ca" = "code_action";
+            "ff" = "format";
+            "K" = "hover";
+          };
         };
 
-        presets = {
-          bottom_search = true;
-          command_palette = true;
-          long_message_to_split = true;
-          inc_rename = true;
-          lsp_doc_border = false;
+        servers = {
+          nil_ls = {
+            enable = true;
+            settings = {
+              formatting.command = ["alejandra" "--quiet"];
+            };
+          };
+          bashls.enable = true;
+          helm_ls.enable = true;
+          nushell.enable = true;
+          # sourcekit.enable = true;
+          terraformls.enable = true;
+          yamlls.enable = true;
+          jsonls.enable = true;
+          gopls.enable = true;
+          eslint.enable = true;
+          ruff.enable = true;
+          # dartls.enable = true;
         };
+      };
+
+      lsp-format = {
+        enable = true;
+      };
+
+      # vim.lsp.set_log_level("debug")
+      # extraConfigLua = ''
+      #   require'lspconfig'.sourcekit.setup {
+      #     cmd = {"/usr/bin/sourcekit-lsp"}
+      #   }
+      # '';
+
+      lspkind = {
+        enable = true;
+        cmp = {
+          enable = true;
+        };
+      };
+
+      nvim-lightbulb = {
+        enable = true;
+        settings = {
+          autocmd.enabled = true;
+        };
+      };
+
+      inc-rename = {
+        enable = true;
+      };
+
+      lualine = {
+        enable = true;
+      };
+
+      trouble = {
+        enable = true;
+        settings = {
+          auto_close = true;
+        };
+      };
+
+      noice = {
+        enable = true;
       };
     };
 
-    extraPlugins = with pkgs.vimPlugins; [
-      editorconfig-nvim
-      ron-vim
-      telescope-ui-select-nvim
-      vim-fish
-      vim-nix
-      vim-prettier
-      vim-sensible
-      vim-shellcheck
-      vim-terraform
-      vim-ripgrep
-      vim-swift
-    ];
+    settings = {
+      messages = {
+        view = "mini";
+        viewError = "mini";
+        viewWarn = "mini";
+      };
 
-    extraPackages = with pkgs; [
-      shfmt
-      # yaml-language-server
-      nodePackages.bash-language-server
-      nil
-      # pyright
-      # python310Packages.python-lsp-server
-      # python310Packages.flake8
-      # python310Packages.autopep8
-    ];
+      lsp.override = {
+        "vim.lsp.util.convert_input_to_markdown_lines" = true;
+        "vim.lsp.util.stylize_markdown" = true;
+        "cmp.entry.get_documentation" = true;
+      };
+
+      presets = {
+        bottom_search = true;
+        command_palette = true;
+        long_message_to_split = true;
+        inc_rename = true;
+        lsp_doc_border = false;
+      };
+    };
   };
+
+  extraPlugins = with pkgs.vimPlugins; [
+    editorconfig-nvim
+    ron-vim
+    telescope-ui-select-nvim
+    vim-fish
+    vim-nix
+    vim-prettier
+    vim-sensible
+    vim-shellcheck
+    vim-terraform
+    vim-ripgrep
+    vim-swift
+  ];
+
+  extraPackages = with pkgs; [
+    shfmt
+    # yaml-language-server
+    nodePackages.bash-language-server
+    nil
+    # pyright
+    # python310Packages.python-lsp-server
+    # python310Packages.flake8
+    # python310Packages.autopep8
+  ];
 }
