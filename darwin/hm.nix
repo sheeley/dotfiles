ac71305be6bded4e91338921a3a91691af829452
Johnny Sheeley <sheeley@aigee.org>
2025-05-10 19:08:33 -0700

    VS Code is borked currently

--- darwin/hm.nix
+++ darwin/hm.nix
@@ -1,8 +1,10 @@
 {user, ...}: {
   home-manager.users.${user} = {lib, ...}: {
+    home.sessionVariables.SSH_ASKPASS = "/opt/homebrew/bin/touch2sudo";
+
     imports = [
       # ./programs/ghostty.nix
-      ../programs/vscode.nix
+      # ../programs/vscode.nix
       ./home-darwin-defaults.nix
     ];
   };
